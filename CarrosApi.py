f63555e6ae84f67571b58e93a2cd8b729a05be2c
Gianny Pereira <nicolaspereira990106@hotmail.com>
2023-09-18 19:36:50 -0500

    Actualizacion 2

---
+++ CarrosApi.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET","POST"])
+
+def carros():
+     name = request.headers.get('User-name')
+     method = request.method
+
+     if method == "GET":
+        return f'Tu nombre es{name}'
+     elif method == "POST":
+         data = request.get()
+         print("Hemos reciido u usario")
+         print("Su nombre es", data.get("nombre"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
